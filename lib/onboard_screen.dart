be137ff5dd540d01ad6cab8f577c7cd41498f10c
HKElim <hkemhougbe@gmail.com>
2023-10-17 09:19:00 +0000

    design terminé

---
+++ lib/onboard_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class OnboardScreen extends StatelessWidget {
+  const OnboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFDEEAE8),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 45),
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            Image.asset("assets/images/p1.png"),
+            const SizedBox(height: 70),
+            const Text(
+              "Gets things done with to do",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 58),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Lorem ipsum dolor sit amet consectetur. Enim.",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF30A6AE),
+                shape: const RoundedRectangleBorder(),
+                minimumSize: const Size.fromHeight(52),
+              ),
+              child: const Text(
+                "Get Started",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFFDEEAE8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 90),
+          ],
+        ),
+      ),
+    );
+  }
+}
